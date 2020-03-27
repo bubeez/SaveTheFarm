610a76e9450655194da98696f92f0520b19e0328
Christopher Blount <christopher.blount@outlook.com>
2020-03-27 00:36:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -34,9 +34,14 @@ include "searchSafe.php";
 
 //Display Intro Text
 printHelp();
-echo "\nOk, the game is about to begin. Will you be able to find the golden mouse statuette and save your great-aunt's farm? We wish you good luck!\n**********BEGIN GAME***********\n";
+echo "\n<p>Ok, the game is about to begin. Will you be able to find the golden mouse statuette and save your great-aunt's farm? 
+We wish you good luck!</p>\n<p>**********BEGIN GAME***********</p>\n";
 
-echo "\nHello there. It's been a harrowing few weeks. First your toenail issue, and now Great-Aunt Natasha's emu farm is in danger of being reposessed! So here you are---after a brief stopover at the Mayo clinic---in uncle Boris's remote cabin in the heart of the Terror Woods. Family legend holds that a golden statue of immense value is hidden somewhere within these walls. Unfortunately, you have a lot of stuff to do today, so you can only devote about 25 minutes to finding the statue. I guess we'll see what happens ;) \n\n";
+echo "\n<p>Hello there. It's been a harrowing few weeks. First your toenail issue, and now Great-Aunt Natasha's emu farm is in 
+danger of being reposessed! So here you are---after a brief stopover at the Mayo clinic---in uncle Boris's remote cabin in the 
+heart of the Terror Woods. Family legend holds that a golden statue of immense value is hidden somewhere within these walls. 
+Unfortunately, you have a lot of stuff to do today, so you can only devote about 25 minutes to finding the statue. I guess we'll 
+see what happens ;) </p>\n\n";
 
 
 // Play 25 rounds
