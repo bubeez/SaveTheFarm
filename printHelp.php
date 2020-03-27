24c279b3eb7f6ba51b0d1917aed30e7f3dd1b2ea
Christopher Blount <christopher.blount@outlook.com>
2020-03-27 13:56:33 -0500

    Update printHelp.php

--- printHelp.php
+++ printHelp.php
@@ -3,7 +3,7 @@
 // Display Help Text
 function printHelp(){
  $commands=["help", "look around", "go", "put on glasses", "take off glasses", "put on contacts", "take off contacts", "eat", "cook", "pee", "pick mushrooms", "move cupboard", "search safe"];
- echo "\n<h1>Welcome</h1>\n<h4>Welcome to the adventure!</h4>
+ echo "\n<h1>Welcome to the adventure!</h1>\n
  <p>You have 25 total rounds before the game ends. On each round you can type one of the following commands:</p>\n";
  echo implode("\n<br> ", $commands);
  echo "\n<p>These commands have to be entered precisely, with no extra spaces. The \"help\" command will re-print this message.</p>\n";
